55d19d605c6c6ad3b08370e30c5949e02f956486
Lucas Schiochet <lukasdgs23dg@gmail.com>
2025-12-28 19:26:15 -0300

    Tirar o fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -29,23 +29,23 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::loginView(function () {
-            return view('auth.login');
-        });
-        Fortify::registerView(function () {
-            return view('auth.register');
-        });
-        Fortify::requestPasswordResetLinkView(function () {
-            return view('auth.forgot-password');
-        });
-        Fortify::resetPasswordView(function ($request) {
-            return view('auth.reset-password', ['request' => $request]);
-        });
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-        Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
+        // Fortify::loginView(function () {
+        //     return view('auth.login');
+        // });
+        // Fortify::registerView(function () {
+        //     return view('auth.register');
+        // });
+        // Fortify::requestPasswordResetLinkView(function () {
+        //     return view('auth.forgot-password');
+        // });
+        // Fortify::resetPasswordView(function ($request) {
+        //     return view('auth.reset-password', ['request' => $request]);
+        // });
+        // Fortify::createUsersUsing(CreateNewUser::class);
+        // Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        // Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        // Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+        // Fortify::redirectUserForTwoFactorAuthenticationUsing(RedirectIfTwoFactorAuthenticatable::class);
 
         RateLimiter::for('login', function (Request $request) {
             $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ Route::middleware(['auth'])->group(function () {
     Route::redirect('/', '/admin/dashboard')->name('home');
 });
 
+Route::get('login', function () {
+    return redirect()->route('backpack.auth.login');
+})->name('login');
+
 // Catch-all route for pages
 Route::get('{slug}', [App\Http\Controllers\PageController::class, 'index'])->where('slug', '.*');
