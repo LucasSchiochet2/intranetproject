54ca125de8c55510bd37d70ee452b30c07682a13
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-14 10:15:22 -0300

    Ajuste https para online

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,12 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('production') || str_contains(config('app.url'), 'https')) {
             URL::forceScheme('https');
+
+            // Fix for Basset/Storage creating http links if APP_URL is http
+            $publicUrl = config('filesystems.disks.public.url');
+            if ($publicUrl && str_contains($publicUrl, 'http://')) {
+                config(['filesystems.disks.public.url' => str_replace('http://', 'https://', $publicUrl)]);
+            }
         }
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(append: [
             \App\Http\Middleware\CheckTenant::class,
         ]);
-        
+
         $middleware->alias([
             'frontend.only' => \App\Http\Middleware\EnsureFrontendRequest::class,
         ]);
