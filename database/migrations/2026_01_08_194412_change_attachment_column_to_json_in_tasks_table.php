f9e4f6d0fe7ed6682f988da9af00b763a9f09834
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-14 09:28:03 -0300

    Update 2026_01_08_194412_change_attachment_column_to_json_in_tasks_table.php

--- database/migrations/2026_01_08_194412_change_attachment_column_to_json_in_tasks_table.php
+++ database/migrations/2026_01_08_194412_change_attachment_column_to_json_in_tasks_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,9 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('tasks', function (Blueprint $table) {
-            $table->json('attachment')->nullable()->change();
-        });
+        DB::statement('ALTER TABLE tasks ALTER COLUMN attachment TYPE json USING attachment::json');
+        DB::statement('ALTER TABLE tasks ALTER COLUMN attachment DROP NOT NULL');
     }
 
     /**
@@ -21,8 +21,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('tasks', function (Blueprint $table) {
-            $table->string('attachment')->nullable()->change();
-        });
+        DB::statement('ALTER TABLE tasks ALTER COLUMN attachment TYPE varchar(255) USING attachment::varchar');
     }
 };
