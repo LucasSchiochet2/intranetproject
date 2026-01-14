32ca0ad5c8397cf7cc3a03227da400d392c5688f
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-14 11:50:39 -0300

    a

--- AppServiceProvider.php
+++ AppServiceProvider.php
@@ -17,10 +17,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
+   public function boot(): void
+{
+    // Se estivermos em produção (Railway)
+    if ($this->app->environment('production')) {
+        // 1. Força os links gerados a serem https://
+        URL::forceScheme('https');
+
+        // 2. Força a URL raiz a ser a que você definiu no .env
+        // Isso impede que o Laravel use "http://localhost" ou o IP interno
+        if (config('app.url')) {
+            URL::forceRootUrl(config('app.url'));
         }
     }
+}
 }
