b9662a02cb314ccfa41bb6a2b06d90a9fb8ae11a
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-15 16:10:37 -0300

    logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Criar Conta - {{ config('app.name', 'Intranet') }}</title>
+    <title>Criar Conta - ls intranet</title>
+    <link rel="icon" type="image/png" href="/logo.png">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700&display=swap" rel="stylesheet" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
---
+++ resources/views/vendor/backpack/theme-coreuiv4/layouts/app.blade.php
@@ -0,0 +1,8 @@
+{{-- resources/views/vendor/backpack/theme-coreuiv4/layouts/app.blade.php --}}
+@extends('backpack::layouts.app')
+
+@section('head')
+    @parent
+    <title>ls intranet</title>
+    <link rel="icon" type="image/png" href="/logo.png">
+@endsection
