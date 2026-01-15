8a2eeb5b77b4c0528d774585c6050b820374281f
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-15 12:08:05 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,12 +5,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/vendor/autoload.php';
+// ✔ AUTOLAOD CORRETO
+require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+// ✔ BOOTSTRAP CORRETO
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
