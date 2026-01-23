a5c7ff5f0e90ab2da15edff5eacbf1338c3d1c4b
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-23 16:45:12 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,19 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 
 Storage::disk('s3')->put('basset/.basset', '');
-// Registration Routes
-Route::get('register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+// Registration Routes (subdomain aware)
+Route::domain('{subdomain}.' . env('APP_DOMAIN'))->group(function () {
+    Route::get('register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
 
-Route::middleware(['auth'])->group(function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::redirect('/', '/admin/dashboard')->name('home');
+    });
 
-    Route::redirect('/', '/admin/dashboard')->name('home');
+    Route::get('login', function () {
+        return redirect()->route('backpack.auth.login');
+    })->name('login');
+
+    // Catch-all route for pages
+    Route::get('{slug}', [App\Http\Controllers\PageController::class, 'index'])->where('slug', '.*');
 });
-
-Route::get('login', function () {
-    return redirect()->route('backpack.auth.login');
-})->name('login');
-
-// Catch-all route for pages
-Route::get('{slug}', [App\Http\Controllers\PageController::class, 'index'])->where('slug', '.*');
