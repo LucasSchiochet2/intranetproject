151b8db970f196751185dac799acf35b51618da2
Lucas Schiochet <lukasdgs23dg@gmail.com>
2026-01-28 15:03:12 -0300

    Update DashboardController.php

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -127,7 +127,7 @@ class DashboardController extends Controller
      */
     public function show(string $id)
     {
-        $dashboard = Dashboard::find($id)->with('tasks');
+        $dashboard = Dashboard::with(['collaborators', 'tasks'])->find($id);
 
         if (!$dashboard) {
             return response()->json(['message' => 'Dashboard not found'], 404);
